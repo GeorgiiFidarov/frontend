943abe8d12e411a1d9dcc826f833c761c79c1a5e
georgii <fidarov371@gmail.com>
2023-05-02 18:35:08 +0300

    Salam

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-async function jsonData(){
-    const responce = await fetch("http://localhost:8080/category/");
-    const jsonData = await responce.json();
-    console.log(jsonData)
-}
+const container = document.querySelector('.content');
+console.log(container);
+
+const dataTable = () => {
+    fetch('http://localhost:8080/category/')
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+
+        container.innerHTML = data.categories.map(
+            (category) => 
+            `
+            <div class="card">
+                <img src=${category.imageUrl} alt="">
+            </div>
+            `
+        );
+    })
+    .catch((err)=>console.log(err));
+};
+
+dataTable();
