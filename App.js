1ddece2487f5b57fa02abaa64c829f58f11bd2ee
georgii <fidarov371@gmail.com>
2023-05-03 22:59:13 +0300

    commit

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
+const searchInput = document.querySelector('.search');
+
+var searchs = [];
+
+searchInput.addEventListener('input', e=>{
+    const value = e.target.value.toLowerCase();
+    searchs.forEach(search=>{
+        const isVisible = 
+        search.categoryName.toLowerCase().includes(value);
+    const cardElem = document.querySelector("#"+search.categoryName);
+    
+    cardElem.classList.toggle('hide',!isVisible);
+
+    })
+})
+
+/*************************************************************************/
 const container = document.querySelector('.content');
-console.log(container);
 
 const dataTable = () => {
     fetch('http://localhost:8080/category/')
     .then(res=>res.json())
     .then(data=>{
-        console.log(data.categories.map(category=>category.imageUrl));
-
         container.innerHTML = data.categories.map(
             (category) => 
             `
-            <div class="card">
-                <img src=${category.imageUrl} alt="">
+            <div class="card" id="${category.categoryName}">
+                <button>
+                    <img src=${category.imageUrl} alt="">
+                </button>
             </div>
             `
         ).join("");
+        searchs = data.categories;
+        
     })
     .catch((err)=>console.log(err));
 };
-
 dataTable();
+/*************************************************************************/
