c5be7d98944d89a5e9d678092cddae85d811765f
georgii <fidarov371@gmail.com>
2023-06-11 14:32:35 +0300

    Revert "first commit"
    This reverts commit 6318fbc03e02d40a8635be4b652acef7dd9ad6af.

--- App.js
+++ App.js
@@ -1,57 +1,40 @@
 const searchInput = document.querySelector('.search');
 
 var searchs = [];
-if(searchInput){
-    searchInput.addEventListener('input', e=>{
-        const value = e.target.value.toLowerCase();
-        searchs.forEach(search=>{
-            const isVisible = 
-            search.categoryName.toLowerCase().includes(value);
-        const cardElem = document.querySelector("#"+search.categoryName);
-        
-        cardElem.classList.toggle('hide',!isVisible);
+
+searchInput.addEventListener('input', e=>{
+    const value = e.target.value.toLowerCase();
+    searchs.forEach(search=>{
+        const isVisible = 
+        search.categoryName.toLowerCase().includes(value);
+    const cardElem = document.querySelector("#"+search.categoryName);
     
-        })
+    cardElem.classList.toggle('hide',!isVisible);
+
     })
-}
+})
 
 /*************************************************************************/
 const container = document.querySelector('.content');
-if(container){
-    const dataTable = () => {
-        fetch('http://localhost:8080/category/')
-        .then(res=>res.json())
-        .then(data=>{
-            container.innerHTML = data.categories.map(
-                (category) => 
-                `
-                <div class="card" id="${category.categoryName}">
-                    <a href="/ProductPage.html">
-                        <img onclick="javascript:getId(${category.id})" src=${category.imageUrl} alt="${category.id}">
-                    </a>
-                </div>
-                `
-            ).join("");
-            searchs = data.categories;
-            console.log(searchs)
-            
-        })
-        .catch((err)=>console.log(err));
-    };
-    dataTable();
-}
-/*************************************************************************/
-function getId(id){
-    sessionStorage.setItem('categoryId',id)
-}
-const products = document.querySelector('.products');
 
-if(products){
-    const product =()=>{
-        let categoryId = sessionStorage.getItem('categoryId');
-        fetch(`http://localhost:8080/product/category/${categoryId}/products`)
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-    }
-    product();
-}
+const dataTable = () => {
+    fetch('http://localhost:8080/category/')
+    .then(res=>res.json())
+    .then(data=>{
+        container.innerHTML = data.categories.map(
+            (category) => 
+            `
+            <div class="card" id="${category.categoryName}">
+                <button>
+                    <img src=${category.imageUrl} alt="">
+                </button>
+            </div>
+            `
+        ).join("");
+        searchs = data.categories;
+        
+    })
+    .catch((err)=>console.log(err));
+};
+dataTable();
+/*************************************************************************/
